08742e2c4b3e3ce00118ecf41d70fe6e70222f5f
mx3030 <mxpayne3030@gmail.com>
2023-04-09 06:43:57 +0200

    fast

--- code.js
+++ code.js
@@ -111,7 +111,6 @@ function onSnapEnd () {
     board.position(game.fen())
 }
 
-
 var config = {
     draggable: true,
     position: 'start',
@@ -149,12 +148,12 @@ function startTraining(){
     var file = fileInput.files[0]
     var reader = new FileReader()
     reader.addEventListener('load', function() {
-        /*run code if FileReader can get input because user selected pgn file*/
-        /*get pgn file as string to work with pgn-parser libary from github*/
+        //run code if FileReader can get input because user selected pgn file
+        //get pgn file as string to work with pgn-parser libary from github
         var pgn = reader.result;
-        /*use pgn-parser to split games*/
+        //use pgn-parser to split games
         var games = splitGames(pgn)
-        /*save every game pgn string in array pgn_strings*/
+        //save every game pgn string in array pgn_strings
         pgn_strings = []
         for(var i=0;i<games.length;i++){
             var singleGame = games[i].all
@@ -162,13 +161,50 @@ function startTraining(){
         }
         pgn_strings_random = shuffleArray(pgn_strings)
         pgn_strings_random_length = pgn_strings_random.length
-        /*start with first random puzzle*/
+        //start with first random puzzle
         runPgn(pgn_strings_random_index)
     });
     reader.readAsText(file);
 }
 
-/*fetch functino if file should be directly read from github server*/
+
+function startTraining_multipleFiles(){
+    /*from chatGPT and not working*/
+    $('#start').addClass("d-none")
+    $('#progress').removeClass("d-none")
+    var fileInput = document.getElementById('fileInput');
+    var files = fileInput.files
+    var fileNumber = files.length 
+    pgn_strings = []
+    var pgn_strings_random = []
+    var pgn_strings_random_length = 0
+    var pgn_strings_random_index = 0
+
+    for(var i=0;i<fileNumber;i++){
+        var reader = new FileReader()
+        reader.onload = (function(index) {
+            return function() {
+                var pgn = reader.result;
+                console.log(pgn)
+                var games = splitGames(pgn)
+                for(var j=0;j<games.length;j++){
+                    var singleGame = games[j].all
+                    pgn_strings.push(singleGame)
+                }
+                if(index===fileNumber-1){
+                    console.log("ich war hier")
+                    pgn_strings_random = shuffleArray(pgn_strings)
+                    pgn_strings_random_length = pgn_strings_random.length
+                    /*start with first random puzzle*/
+                    runPgn(pgn_strings_random_index)
+                }
+            };
+        })(i);
+        reader.readAsText(files[i]);
+    }
+}
+
+/*fetch function if file should be directly read from github server*/
 
 //fetch("pgn/test4.pgn")
 //.then(response => response.text())
